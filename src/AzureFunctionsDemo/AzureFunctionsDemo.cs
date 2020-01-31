c1b4c5a02089ff7f39def7fba8850e51b7c0d8f1
JanDonnermayer <jan@donnermayer.de>
2020-01-31 23:24:58 +0100

    Update AzureFunctionsDemo.cs

--- src/AzureFunctionsDemo/AzureFunctionsDemo.cs
+++ src/AzureFunctionsDemo/AzureFunctionsDemo.cs
@@ -14,7 +14,7 @@ namespace Company.Function
     {
         [FunctionName("AzureFunctionsDemo")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
