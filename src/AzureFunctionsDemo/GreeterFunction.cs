5fa5b7853a26d9c52d07ebea0840e1321057a99e
JanDonnermayer <jan@donnermayer.de>
2020-03-02 14:06:00 +0100

    Added unit test

---
+++ src/AzureFunctionsDemo/GreeterFunction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public static class GreeterFunction
+    {
+        [FunctionName("AzureFunctionsDemo")]
+        public static async Task<IActionResult> ExecuteAsync(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger logger)
+        {
+            logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            try
+            {
+                using var reader = new StreamReader(req.Body);
+                string requestBody = await reader.ReadToEndAsync().ConfigureAwait(false);
+                dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+                var name = data?.name;
+
+                return name != null
+                    ? (ActionResult)new OkObjectResult($"Hello, {name}!")
+                    : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            }
+            catch (System.Exception ex)
+            {
+                logger.LogError(ex, "Request failed!");
+                return new BadRequestObjectResult(ex.ToString());
+            }
+        }
+    }
+}
---
+++ test/AzureFunctionsDemo.Test/UnitTest1.cs
@@ -0,0 +1,49 @@
+using NUnit.Framework;
+using Moq;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Company.Function;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Internal;
+using System.Collections.Generic;
+
+namespace AzureFunctionsDemo.Test
+{
+    public class GreeterFunctionTests
+    {
+        private ILogger logger;
+
+        private HttpRequest request;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.logger = Mock.Of<ILogger>();
+            this.request = Mock.Of<HttpRequest>();
+        }
+
+        [Test]
+        public async Task Test_JsonRequestBody_ReturnsOkObjectResult()
+        {
+            // Arrange
+            using var stream =
+                new MemoryStream(
+                    System.Text.Encoding.UTF8.GetBytes("{\"name\":\"jan\"}")
+                );
+
+            Mock.Get(request)
+                .SetupGet(r => r.Body)
+                .Returns(stream);
+
+            // Act
+            var response = await GreeterFunction
+                .ExecuteAsync(request, logger)
+                .ConfigureAwait(false);
+
+            // Assert
+            Assert.IsInstanceOf<OkObjectResult>(response);
+        }
+    }
+}
